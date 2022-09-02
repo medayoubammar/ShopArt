ba57906b20fce55504a42518d1b40a79741f34a9
AmmarMedAyoub <mohamedayoub.ammar@etudiant-isi.utm.tn>
2022-09-02 15:08:22 -0700

    lastUpdate

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,89 @@
-import React from 'react'
-
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import Contact from "./Contact";
+import Navbar from "./Navbar";
 export default function Card() {
+  const { state } = useLocation();
+  const { item } = state;
+
   return (
-    <div>Card</div>
-  )
+    <>
+      <Navbar />
+
+      <div className="container">
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">
+                  Place an order for :{" "}
+                  <span className="text-primary">{item[1].Title}</span>
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                />
+              </div>
+              <div className="modal-body">
+                <Contact
+                  order={{
+                    Title: item[1].Title,
+                    PhotoURL: item[1].PhotoURL,
+                    Price: item[1].Price,
+                  }}
+                  DBRef={"/Orders"}
+                />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-body">
+          <h5>Title : {item[1].Title}</h5>
+          <img
+            src={item[1].PhotoURL}
+            className="img-fluid rounded"
+            alt="art_work"
+          />
+          <p className="card-text pt-3">
+            {" "}
+            <span className="h5">Description</span> :{item[1].Description}
+          </p>
+          <h4 className="card-text pt-3">
+            Price :<span className="text-success">{item[1].Price}</span> DT
+          </h4>
+          <button
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+            className="btn btn-success w-25"
+          >
+            Place an order
+          </button>
+          <div className="card-footer bg-light ">
+            <Link to="/">
+              <button className="btn btn-link">return to home page</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/components/Login.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.css";
+export default function Login(props) {
+  const {
+    email,
+    password,
+    setEmail,
+    setPassword,
+    handelLogin,
+    emailError,
+    passwordError,
+  } = props;
+  return (
+    <div>
+      <div className="alert alert-warning warning text-center">
+        This is a warning alert ! Only website owners have acess to check Admins
+        dashbord ! if you don't have an admin account please back to home page
+        <br></br>
+        <Link to="/">
+          <p className="text-center">Back to home page</p>
+        </Link>
+      </div>
+      <section className="login">
+        <div className="loginContainer">
+          <h3>Login</h3>
+          <label>Email</label>
+          <input
+            type="text"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <p className="errorMsg">{emailError}</p>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="errorMsg">{passwordError}</p>
+          <div className="btnContainer">
+            <button onClick={handelLogin}>Login</button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
